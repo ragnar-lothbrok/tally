a44278c15de05820b2193c88b24028b8224f5c2d
pm-raghunandan-gupta <raghunandan.gupta@pubmatic.com>
2015-08-07 08:07:26 +0530

    Min Heap issue fixed.

--- src/main/java/com/home/heap/MainClass.java
+++ src/main/java/com/home/heap/MainClass.java
@@ -6,15 +6,24 @@ public class MainClass {
     public static void main(String[] args) {
 
         String str[] = { "Ram", "Shyam", "Rosie", "Mohan", "Barney", "Barney", "Betty", "Bam Bam", "Jetsons", "George",
-                "Flintstones", "Jane", "Elroy", "Judy", "Flintstones", "Rosie", "Bam Bam", "Mohan", "Scooby Doo Gang",
+                "Flintstones", "Jane", "Elroy", "Judy", "Flintstones", "Rosie", "Bam Bam", "Mohan",
                 "Mohan", "Scooby Doo", "Mohan", "Rosie", "Barney", "Scooby Doo" };
-        HeapConstructionService<String> heapConstructionService = new HeapConstructionService<String>();
+        MaxHeapConstructionService<String> heapConstructionService = new MaxHeapConstructionService<String>();
+        
+        MinHeapConstructionService<String> heapMinHeapConstructionService = new MinHeapConstructionService<String>();
         for (String value : str) {
+            if(value.equalsIgnoreCase("Scooby Doo"))
+                System.out.println();
             heapConstructionService.addoToHeap(new HeapNode<String>(value, 1));
+            heapMinHeapConstructionService.addoToHeap(new HeapNode<String>(value, 1));
         }
         System.out.println(heapConstructionService.getHeapNodesList());
         
-        System.out.println(heapConstructionService.getTopHeapNodes(3));
+        System.out.println(heapConstructionService.getTopHeapNodes(6));
+        
+        System.out.println(heapMinHeapConstructionService);
+        
+        System.out.println(heapMinHeapConstructionService.getTopHeapNodes(12));
     }
 
 }
