4023038b07fbf164fe4e1f4f4a92fd6e43853f33
pm-raghunandan-gupta <raghunandan.gupta@pubmatic.com>
2015-10-05 10:34:19 +0530

    Dijkastra also added in shortest path.

--- src/main/java/com/he/graphs/BellmanFordViaAdjacency.java
+++ src/main/java/com/he/graphs/BellmanFordViaAdjacency.java
@@ -1,17 +1,26 @@
 package com.he.graphs;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
 public class BellmanFordViaAdjacency {
 
     public static void main(String[] args) {
 
         int mat[][] = { { 0, 4, 0, 0, -1, 0 }, { 0, 0, -1, 0, -2, 0 }, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 },
                 { 0, 0, 0, -5, 0, 3 }, { 0, 0, 0, 0, 0, 0 } };
-        int adjacency[][] = new int[mat.length + 1][mat.length + 1];
-        initializeMat(mat, adjacency);
+        int mat1[][] = { { 0, 7, 0, 0, 2 }, { 0, 0, 1, 0, 2 }, { 0, 0, 0, 4, 0 }, { 0, 0, 5, 0, 0 }, { 0, 3, 8, 5, 0 } };
+
+        int adjacency[][] = new int[mat1.length + 1][mat1.length + 1];
+        initializeMat(mat1, adjacency);
         int source = 1;
-        int vertexCount = mat.length;
+        int vertexCount = mat1.length;
         calculateSingleSourceShortestPath(adjacency, source, vertexCount);
         calculateAllPairSourceShortestPath(adjacency, source, vertexCount);
+        calculateSingleSourceShortestPathDijkastra(adjacency, source, vertexCount);
     }
 
     private static void initializeMat(int[][] adMat, int adjacency[][]) {
@@ -60,9 +69,11 @@ public class BellmanFordViaAdjacency {
             }
         }
 
+        System.out.println("Bellman Ford...");
         for (int vertex = 1; vertex <= vertexCount; vertex++) {
             System.out.println("Distance from " + source + " to " + vertex + " is : " + distance[vertex]);
         }
+        System.out.println();
     }
 
     private static void calculateAllPairSourceShortestPath(int[][] mat, int source, int vertexCount) {
@@ -94,8 +105,56 @@ public class BellmanFordViaAdjacency {
             }
         }
 
+        System.out.println("Floyd Warshall...");
         for (int vertex = 1; vertex <= vertexCount; vertex++) {
             System.out.println("Distance from " + source + " to " + vertex + " is : " + distance[source][vertex]);
+        }
+    }
+
+    private static void calculateSingleSourceShortestPathDijkastra(int[][] mat, int source, int vertexCount) {
+        int distance[] = new int[vertexCount + 1];
+        for (int i = 1; i < mat.length; i++) {
+            distance[i] = Integer.MAX_VALUE;
+        }
+        distance[source] = 0;
+
+        Queue<Integer> queue = new LinkedList<Integer>();
+        queue.add(source);
+        
+        Set<Integer> settled = new HashSet<Integer>();
+
+        while (!queue.isEmpty()) {
+
+            //get node with minimum distance
+            Iterator<Integer> iterator = queue.iterator();
+            int min = Integer.MAX_VALUE;
+            int node = -1;
+            while(iterator.hasNext()){
+               int tempNode = iterator.next();
+                if(distance[tempNode] <= min){
+                    min = distance[tempNode];
+                    node = tempNode;
+                }
+            }
+            queue.remove(node);
+            settled.add(node);
+            
+            //Calculate neighbors distances
+            
+            for (int dest = 1; dest <= vertexCount; dest++) {
+                if(!settled.contains(dest)){
+                    if(mat[node][dest] != Integer.MAX_VALUE){
+                        if(distance[node] + mat[node][dest] < distance[dest]){
+                            distance[dest] = distance[node] + mat[node][dest];
+                        }
+                        queue.add(dest);
+                    }
+                }
+            }
+        }
+        System.out.println("\nDijkastra...");
+        for (int vertex = 1; vertex <= vertexCount; vertex++) {
+            System.out.println("Distance from " + source + " to " + vertex + " is : " + distance[vertex]);
         }
     }
 
